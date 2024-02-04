82a94aabc812e8102b1ab85faa47283722f13d92
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-02-04 11:09:39 -0600

    improve cli

--- cli/index.js
+++ cli/index.js
@@ -1,21 +1,31 @@
 #!/usr/bin/env node
 
+const path = require("path");
 const { program } = require("commander");
 const { execSync } = require("child_process");
 const chalk = require("chalk");
+const prompt = require("prompt-sync")();
+
+const currentDirectory = process.cwd();
 
 program
+  .arguments("[name]")
   .description(
     `tool to quickly generate a minimalistic template with module bundling, hmr, typescript, tailwind & sass configured`
   )
-  .action(() => {
-    const name = "my-app";
-    const command = `npx degit https://github.com/maxhu08/neutrino-template/templates/default ${name}`;
+  .action((name) => {
+    if (!name) {
+      name = prompt(`${chalk.blue("?")} enter the app name: `);
+    }
+    const destinationFolderPath = path.join(currentDirectory, name);
+
+    const command = `npx degit https://github.com/maxhu08/neutrino-template/templates/default ${destinationFolderPath}`;
+
     try {
-      const output = execSync(command, { encoding: "utf-8" });
-      console.log(output);
+      execSync(`${command} > /dev/null 2>&1`, { encoding: "utf-8" });
+      console.log(`🌌 template cloned into ${destinationFolderPath}`);
     } catch (err) {
-      console.error(`${chalk.red("✘")} err cloning template ${err}`);
+      console.error(`${chalk.red("✘")} err cloning template: ${err}`);
     }
   });
 
