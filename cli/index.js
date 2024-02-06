8b114bdb957b3dab6d895347a06f7ae8600eb0cd
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-02-06 16:30:25 -0600

    fix

--- cli/index.js
+++ cli/index.js
@@ -2,7 +2,7 @@
 
 const path = require("path");
 const { program } = require("commander");
-const { execSync } = require("child_process");
+const { exec } = require("child_process");
 const chalk = require("chalk");
 const prompt = require("prompt-sync")();
 
@@ -21,12 +21,14 @@ program
 
     const command = `npx degit https://github.com/maxhu08/create-neutrino-app/templates/default ${destinationFolderPath}`;
 
-    try {
-      execSync(`${command} > /dev/null 2>&1`, { encoding: "utf-8" });
+    exec(`${command} > /dev/null 2>&1`, { encoding: "utf-8" }, (err) => {
+      if (err) {
+        console.error(`${chalk.red("✘")} err cloning template: ${err}`);
+        process.exit(1);
+      }
+
       console.log(`🌌 template cloned into ${destinationFolderPath}`);
-    } catch (err) {
-      console.error(`${chalk.red("✘")} err cloning template: ${err}`);
-    }
+    });
   });
 
 program.parse(process.argv);
