66b5d2f0eb20a30fba33563d20ad1e56b18f7ed6
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-02-11 09:10:53 -0600

    start adding other templates

--- cli/steps/clone.js
+++ cli/steps/clone.js
@@ -6,7 +6,7 @@ const chalk = require("chalk");
 const cloneTemplate = async ({ name }, callback) => {
   const destinationFolderPath = path.join(currentDirectory, name);
 
-  const command = `npx degit https://github.com/maxhu08/create-neutrino-app/templates/default ${destinationFolderPath}`;
+  const command = `npx degit https://github.com/maxhu08/create-neutrino-app/templates/typescript-taildwind-sass ${destinationFolderPath}`;
 
   exec(`${command} > /dev/null 2>&1`, { encoding: "utf-8" }, (err) => {
     if (err) {
